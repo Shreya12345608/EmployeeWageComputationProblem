383818f02e2510851a23c55f8158439100baebb3
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-01 19:05:19 +0530

    Check Employee is Present or Absent

--- CheckEmpIsPresentOrabsent.sh
+++ CheckEmpIsPresentOrabsent.sh
@@ -1,3 +1,42 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program";
 
+
+function attendance()
+{
+	Random=$((RANDOM%2))
+	if [ $Random -eq 1 ]
+	 then
+		echo "Employee is present";
+	 else
+		echo "Employee is absent";
+	fi
+}
+
+function main()
+{
+
+attendance
+
+}
+
+main
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3))
+
+
+if [ $isFullTime -eq $randomCheck ];
+then
+       empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+      empHrs=8;
+
+else
+      empHrs=0;
+fi
+
+salary=$(($empHrs * $empRatePerHr));
